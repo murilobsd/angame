9c498ad98f570b07372013dfc7f3a802f3ac9494
murilo ijanc <mbsd@m0x.ru>
2024-06-04 18:52:38 -0300

    feat: Create components, query, run plugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Murilo Ijanc" {
+            name.0 = "Elaina Hume".to_string();
+            break;
+        }
+    }
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Ana Luisa".to_string())));
+    commands.spawn((Person, Name("Murilo Ijanc".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
 }
